01c50f6eaf6055b6b403374819fdfba283e13fb8
DungLouis <phamdung140703@gmail.com>
2024-04-06 21:59:31 +0700

    video17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,11 +2,18 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: "dung",
+        age: 21,
+        address: "Thanh Hoa",
+    };
     render() {
         return (
             <div>
                 fuck you
                 {Math.random()}
+                toi ten la: {this.state.name}
+                tuoi: {this.state.age}
             </div>
         );
     }
