09e6ccf761ef43db555dfe8bb42a700fde88b7de
DungLouis <phamdung140703@gmail.com>
2024-04-06 21:43:13 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                fuck you
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
