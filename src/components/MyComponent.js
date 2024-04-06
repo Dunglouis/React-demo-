61a4682214ceb85705a562ff2c7bb69f9d2aa5bb
DungLouis <phamdung140703@gmail.com>
2024-04-07 00:53:57 +0700

    video 23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,14 +1,16 @@
 
 import React from "react";
 import Userinfor from "./Userinfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
 
     render() {
+
         return (
             <div>
-
                 <Userinfor></Userinfor>
+                <DisplayInfor name="Pham Dung" age={21} />
             </div>
 
         );
