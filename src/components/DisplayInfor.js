1545a8c6d6f777a306a83f96fba2df1ecd527a0d
DungLouis <phamdung140703@gmail.com>
2024-04-07 03:30:03 +0700

    video 25

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,27 +1,39 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+    state = {
+        isListUser: true
+    }
+    showList = () => {
+        this.setState({ isListUser: !this.state.isListUser })
+    }
     render() {
         const { listUser } = this.props;
         return (
             <div>
-                {
-                    listUser.map((user) => {
-                        return (
-                            <div>
-                                <div key={user.id}>
-                                    <div>My name: {user.name}</div>
-                                    <div>My age: {user.age}</div>
-                                </div>
-                            </div>
-                        )
-                    })
+                <div>
+                    <span onClick={() => { this.showList() }}>
+                        {this.state.isListUser === true ? "high list" : "show list"}
+                    </span>
+                </div>
+                {this.state.isListUser &&
+                    <div>
+                        {
+                            listUser.map((user) => {
+                                console.log(user);
+                                return (
+                                    <div>
+                                        <div key={user.id} className={user.age > 20 ? "red" : "green"}>
+                                            <div>My name: {user.name}</div>
+                                            <div>My age: {user.age}</div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 }
             </div>
-            // <div>
-            //     <div>My name: {}</div>
-            //     <div>My age: {age}</div>
-            // </div>
         )
     }
 }
